11b89f86bf341aca948d75a13066c647a78054ad
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-06-23 18:12:12 +0200

    Improve test.sh

--- test.sh
+++ test.sh
@@ -2,14 +2,37 @@
 
 INSTANCE_IP=$(cat instance_ip)
 
-r=$(curl -s -Ik http://$INSTANCE_IP | grep "HTTP/1.1")
+a_c=0
 
-echo "IP : $INSTANCE_IP"
-echo "RESPONSE : $r"
+test_ip() {
+	INSTANCE_IP=$1
+	r=$(curl -s -Ik http://$INSTANCE_IP | grep "HTTP/1.1")
+	
+	a_c=$(($a_c+1))
 
-if [[ "$r" =~ "200 OK" ]]; then
-	echo "TEST OK"
-else
-	echo "TEST FAILED"
-	exit 1
-fi
+
+	if [[ "$r" =~ "200 OK" ]]; then
+		echo "Attempt n°$a_c : $r (OK)"
+		return 0
+	else
+		echo "Attempt n°$a_c : $r (FAILED)"
+		return 1
+	fi
+}
+
+
+for i in {1..6}; do
+	echo "IP : $INSTANCE_IP"
+	echo ""
+
+	test_ip $INSTANCE_IP
+
+	if [ $? -eq 0 ]; then
+		exit 0
+	elif [ $i -eq 6 ]; then
+		echo "Can't access to the webserver ($INSTANCE_IP)"
+		exit 1
+	fi
+
+	sleep 10
+done
