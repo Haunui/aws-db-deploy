5dd5c5e085d1981e1464b80879c6c5919f4fd1f1
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-06-23 17:56:57 +0200

    Improve messages

--- test.sh
+++ test.sh
@@ -4,11 +4,12 @@ INSTANCE_IP=$(cat instance_ip)
 
 r=$(curl -s -Ik http://$INSTANCE_IP | grep "HTTP/1.1")
 
-echo "$r"
+echo "IP : $INSTANCE_IP"
+echo "RESPONSE : $r"
 
 if [[ "$r" =~ "200 OK" ]]; then
-	echo "curl test ok."
+	echo "TEST OK"
 else
-	echo "curl test failed."
+	echo "TEST FAILED"
 	exit 1
 fi
