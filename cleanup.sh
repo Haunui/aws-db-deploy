208b657d142af2cf69b6d346e37e62f9c317b0d3
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-06-23 16:41:11 +0200

    Improve deploy and cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,43 @@
 #!/usr/bin/bash
 
-INSTANCE_ID=$1
+shutdown_instance() {
+	STATE=$1
+	INSTANCE_ID=$2
 
-if [ -z "$INSTANCE_ID" ]; then
-	echo "Usage: $0 <instance_id>"
-	exit 1
+	while [[ $STATE != terminated ]]; do
+		echo "Waiting for '$INSTANCE_ID' to shutdown.. (status : $STATE)"
+		STATE=$(aws ec2 terminate-instances --instance-ids $INSTANCE_ID | jq ".TerminatingInstances[] | select(.InstanceId==\"$INSTANCE_ID\") | .CurrentState.Name" | sed 's/"//g')
+		sleep 5
+	done
+
+	echo "Waiting for '$INSTANCE_ID' to shutdown.. (status : $STATE)"
+}
+
+while IFS= read -r line; do
+	INSTANCE_ID=$(echo "$line" | jq '.InstanceId' | sed 's/"//g')
+	STATE=$(echo "$line" | jq '.State.Name' | sed 's/"//g')
+
+	if [[ $STATE != terminated ]]; then
+		f=1
+		for i in {0..20}; do
+			if [ -z "${pids[${i}]}" ]; then
+				shutdown_instance "$STATE" "$INSTANCE_ID" &
+				pids[${i}]=$!
+				break
+			fi
+		done
+		
+	fi 
+done < <(aws ec2 describe-instances --filters Name=tag:Name,Values=HSAINT-instance --query 'Reservations[*]' | jq -c '.[].Instances[]')
+
+sleep 3
+
+for pid in ${pids[@]}; do
+	wait $pid
+done
+
+if [ -z "$f" ]; then
+	echo "Nothing to do."
+else
+	echo "Cleanup done."
 fi
-
-aws ec2 terminate-instances --instance-ids $INSTANCE_ID
--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,13 @@ KEY_NAME="HSAINT-keypair"
 SG_ID="sg-0b84d36f2701b4e1a"
 SUBNET_ID="subnet-0bcc3e037257089ac"
 
-aws ec2 run-instances --image-id "$IMAGE_ID" --instance-type t2.micro --key-name "$KEY_NAME" --security-group-ids $SG_ID --placement AvailabilityZone=$SUBNET_0_ZONE --subnet-id $SUBNET_ID --network-interfaces "AssociatePublicIpAddress=true,SubnetId=$SUBNET_ID,DeviceIndex=0" --count 1 --block-device-mappings 'DeviceName=/dev/xvda,Ebs={DeleteOnTermination=false,VolumeSize=8}' --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='HSAINT-instance'}]" --user-data "$(cat user_data)"
+INSTANCE_ID=$(aws ec2 run-instances --image-id "$IMAGE_ID" --instance-type t2.micro --key-name "$KEY_NAME" --security-group-ids $SG_ID --placement AvailabilityZone=$SUBNET_0_ZONE --subnet-id $SUBNET_ID --network-interfaces "AssociatePublicIpAddress=true,SubnetId=$SUBNET_ID,DeviceIndex=0" --count 1 --block-device-mappings 'DeviceName=/dev/xvda,Ebs={DeleteOnTermination=false,VolumeSize=8}' --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='HSAINT-instance'}]" --user-data "$(cat user_data)" --query 'Instances[0].InstanceId' | sed 's/"//g')
+
+echo "Instance '$INSTANCE_ID' starting initialized"
+sleep 2
+
+while [[ $STATE != running ]]; do
+	STATE=$(aws ec2 describe-instances --filters Name=tag:Name,Values=HSAINT-instance --instance-ids $INSTANCE_ID | jq ".Reservations[0].Instances[].State.Name" | sed 's/"//g')
+	echo "Waiting for instance '$INSTANCE_ID' to start.. (status : $STATE)"
+	sleep 10
+done
