f783fdfb1009cfca09141f681cb2dadceaad033a
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-06-24 08:16:22 +0200

    Select database

--- template_files/webapp/db.sql
+++ template_files/webapp/db.sql
@@ -2,5 +2,7 @@ create database if not exists webapp;
 grant all privileges on webapp.* to '{{USER}}'@'localhost' identified by '{{PASSWORD}}';
 flush privileges;
 
+use webapp;
+
 create table french_words(id int auto_increment not null, word varchar(255) not null, primary key(id));
 create table history(id int auto_increment not null,username varchar(255), score integer, game_date date not null, primary key (id));
