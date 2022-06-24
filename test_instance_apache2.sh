202586516c65a05d92004b6cc5212c13321d9e94
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-06-24 02:18:02 +0200

    Remove message when ssh tests success

--- test_instance_apache2.sh
+++ test_instance_apache2.sh
@@ -5,7 +5,7 @@ INSTANCE_IP=$(cat instance_ip)
 SSH_OPTS="-o StrictHostKeyChecking=no"
 SSH_LOGIN="ubuntu@$INSTANCE_IP"
 
-while ! ssh $SSH_OPTS $SSH_LOGIN 'uname -a' 2> /dev/null; do
+while ! ssh $SSH_OPTS $SSH_LOGIN 'uname -a' &> /dev/null; do
 	echo "Waiting for SSH to be UP .."
 	sleep 5
 done
