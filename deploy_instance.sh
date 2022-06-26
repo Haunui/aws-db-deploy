a6961d74c3aaa4016e290d47dcbc372df2d0ddd4
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-06-26 08:40:36 +0200

    Varialibize deploy. infos

--- deploy_instance.sh
+++ deploy_instance.sh
@@ -2,18 +2,13 @@
 
 source functions.sh
 
-IMAGE_ID="ami-09e513e9eacab10c1"
-KEY_NAME="HSAINT-keypair"
-SG_ID="sg-0b84d36f2701b4e1a"
-SUBNET_ID="subnet-0bcc3e037257089ac"
-
 echo "Generate MYSQL_ROOT_PASSWORD"
 MYSQL_ROOT_PASSWORD=$(gen_cred | cut -d':' -f2)
 cat template_files/user_data | sed "s/{{ROOT_PASSWORD}}/$MYSQL_ROOT_PASSWORD/g" > user_data
 echo "$MYSQL_ROOT_PASSWORD" > mysql_root_password
 
 echo "Deploy instance"
-INSTANCE_ID=$(aws ec2 run-instances --image-id "$IMAGE_ID" --instance-type t2.micro --key-name "$KEY_NAME" --security-group-ids $SG_ID --placement AvailabilityZone=$SUBNET_0_ZONE --subnet-id $SUBNET_ID --network-interfaces "AssociatePublicIpAddress=true,SubnetId=$SUBNET_ID,DeviceIndex=0" --count 1 --block-device-mappings 'DeviceName=/dev/xvda,Ebs={DeleteOnTermination=false,VolumeSize=8}' --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='HSAINT-instance'}]" --user-data "$(cat user_data)" --query 'Instances[0].InstanceId' | sed 's/"//g')
+INSTANCE_ID=$(aws ec2 run-instances --image-id "$AWS_DEPLOY_IMAGE_ID" --instance-type t2.micro --key-name "$AWS_DEPLOY_KEY_NAME" --security-group-ids $AWS_DEPLOY_SG_ID --placement AvailabilityZone=eu-west-3c --subnet-id $AWS_DEPLOY_SUBNET_ID --network-interfaces "AssociatePublicIpAddress=true,SubnetId=$AWS_DEPLOY_SUBNET_ID,DeviceIndex=0" --count 1 --block-device-mappings 'DeviceName=/dev/xvda,Ebs={DeleteOnTermination=false,VolumeSize=8}' --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='HSAINT-instance'}]" --user-data "$(cat user_data)" --query 'Instances[0].InstanceId' | sed 's/"//g')
 
 rm -f user_data
 
